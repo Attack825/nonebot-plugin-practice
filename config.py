79c21ab62390b800a22de7ea1ffb8413083233fd
文杰 <2707138687@qq.com>
2023-06-22 21:58:14 +0800

    修改配置

--- config.py
+++ config.py
@@ -1,13 +1,20 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Extra
+from nonebot import get_driver
 
 
-class Config(BaseModel):
-    weather_api_key: str = "5999763e5ea147b09b491ef6700d45a7"
-    weather_command_priority: int = 10
-    weather_plugin_enabled: bool = True
+class Config(BaseModel, extra=Extra.ignore):
+    weather_api_key: str = None
+    weather_command_priority: int = None
+    weather_plugin_enabled: bool = False
 
     @validator('weather_command_priority')
     def weather_command_priority_range(cls, v):
         if isinstance(v, int) and v >= 1:
             return v
         raise ValueError('weather_command_priority must be an integer greater than or equal to 1')
+
+
+plugin_config = Config.parse_obj(get_driver().config)
+WEATHER_API_KEY = plugin_config.weather_api_key
+WEATHER_COMMAND_PRIORITY = plugin_config.weather_command_priority
+WEATHER_PLUGIN_ENABLED = plugin_config.weather_plugin_enabled
