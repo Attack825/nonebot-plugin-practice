44c7a6dcd241b229c0c5f1f5f1a0ef5000f0a296
文杰 <2707138687@qq.com>
2023-06-26 19:37:21 +0800

    测试修改

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,22 @@
-import logging
-
 import pytest
 import nonebot
-from nonebot.adapters.onebot.v11 import Adapter
-from os import getenv
+# 导入适配器
+from nonebot.adapters.console import Adapter as ConsoleAdapter
+import os
+from nonebug import NONEBOT_INIT_KWARGS
+
+os.environ["ENVIRONMENT"] = "prod"
+
+
+def pytest_configure(config: pytest.Config):
+    config.stash[NONEBOT_INIT_KWARGS] = {"secret": os.getenv("INPUT_SECRET")}
 
 
 @pytest.fixture(scope="session", autouse=True)
 def load_bot():
     # 加载适配器
     driver = nonebot.get_driver()
-    driver.register_adapter(Adapter)
-
-    config = nonebot.get_driver().config
-
-    # 加载配置文件
-    config.weather_api_key = getenv("WEATHER_API_KEY")
-    config.weather_command_priority = 10
-    logging.info(config.weather_command_priority)
-    config.weather_plugin_enabled = bool(getenv("WEATHER_PLUGIN_ENABLED"))
+    driver.register_adapter(ConsoleAdapter)
 
     # 加载插件
-    # nonebot.load_plugin("nonebot_plugin_practice")
+    nonebot.load_from_toml("D:\\Project\QQbot\\bot_xiaojie\\xiaojie\\pyproject.toml")
--- tests/test_practice.py
+++ tests/test_practice.py
@@ -1,20 +1,40 @@
+from datetime import datetime
+
 import pytest
 from nonebug import App
-from nonebot import get_driver
+from nonebot.adapters.console import User, Message, MessageEvent
+
+
+# @pytest.mark.asyncio
+# async def test_practice(app: App):
+#     from xiaojie.plugins.nonebot_plugin_practice import Weather, CityNotFoundError
+#
+#     async with app.test_server():
+#         config = get_driver().config
+#         # api_key = config.weather_api_key
+#         api_key = "5999763e5ea147b09b491ef6700d45a7"
+#         w_data = Weather(location="上海", api_key=api_key)
+#         try:
+#             await w_data.get_weather()
+#             assert w_data.location == "上海"
+#             assert w_data.location_id == "101020100"
+#         except CityNotFoundError:
+#             pass
 
 
 @pytest.mark.asyncio
-async def test_practice(app: App):
-    from xiaojie.plugins.nonebot_plugin_practice import Weather, CityNotFoundError
+async def test_weather(app: App):
+    from xiaojie.plugins.nonebot_plugin_practice import weather
 
-    async with app.test_server():
-        config = get_driver().config
-        # api_key = config.weather_api_key
-        api_key = "5999763e5ea147b09b491ef6700d45a7"
-        w_data = Weather(location="上海", api_key=api_key)
-        try:
-            await w_data.get_weather()
-            assert w_data.location == "上海"
-            assert w_data.location_id == "101020100"
-        except CityNotFoundError:
-            pass
+    event = MessageEvent(
+        time=datetime.now(),
+        self_id="test",
+        message=Message("/天气 北京"),
+        user=User(id=123465789),
+    )
+    async with app.test_matcher(weather) as ctx:
+        bot = ctx.create_bot()
+        ctx.receive_event(bot, event)
+        ctx.should_call_send(event, message="今天北京天气是小雨，最高温度34℃，最低温度24℃", result=None)
+        ctx.should_finished(weather)
+# 修改后的配置无法导入测试
